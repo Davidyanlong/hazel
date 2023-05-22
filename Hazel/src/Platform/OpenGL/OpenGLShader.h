e137bf61bfa9ab7ac2b50faa985bc21783377e36
yanlong.yang <yyl_love_you@126.com>
2023-05-22 21:41:33 +0800

    Shaders can now be read from files

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -3,12 +3,16 @@
 #include "Hazel/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO:REMOVE!
+typedef unsigned int GLenum;
+
 namespace Hazel {
 
 	class OpenGLShader : public Shader
 	{
 	public:
-		OpenGLShader(const std::string& vertexSrc,  const std::string&  fragmentSrc);
+		OpenGLShader(const std::string& filePath);
+		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
 		virtual void Bind() const override;
@@ -24,7 +28,10 @@ namespace Hazel {
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
-
+	private:
+		std::string	ReadFile(const std::string& filePath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSource);
 	private:
 		uint32_t m_RendererID;
 	};
